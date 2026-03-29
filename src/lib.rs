a317a5cc10e7d42aa7217515e99e678fed4fad43
William Carson <wcarson@bardytechnologies.com>
2026-03-29 12:39:51 -0700

    Scheduled

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub use bevy_gearbox_core::*;
+pub use bevy_gearbox_schedule::*;
+
+// Core is still available for consumers that need it during migration.
+pub mod core {
+    pub use bevy_gearbox_core::*;
+}
 
 #[cfg(feature = "server")]
 pub mod server {
